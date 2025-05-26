070de6f963a424e3d7c798d57013ce0a23abe087
franzholmes822 <franzholmes822@gmail.com>
2025-05-26 14:40:02 +0000

    Add 38.py

---
+++ src/38.py
@@ -0,0 +1,21 @@
+def print_numbers():
+    """
+    Prints numbers from 1 to 10.
+    
+    Example usage:
+        >>> print_numbers()
+        1
+        2
+        3
+        4
+        5
+        6
+        7
+        8
+        9
+        10
+    """
+    for i in range(1, 11):
+        print(i)
+
+print_numbers()
