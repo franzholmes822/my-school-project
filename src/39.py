ba2a4b031c000159e3db918dfeb06656d68bac10
franzholmes822 <franzholmes822@gmail.com>
2025-05-27 12:09:13 +0000

    Add 39.py

---
+++ src/39.py
@@ -0,0 +1,8 @@
+def add_numbers(a: int, b: int) -> int:
+    return a + b
+
+def calculate_square_root(x: float) -> float:
+    return x ** 0.5
+
+def print_info(name: str, age: int, grade: str = "Freshman") -> None:
+    print(f"Hello, {name}! You are {age} years old and you're in the {grade}.")
